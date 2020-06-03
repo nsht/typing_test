f6431980be22b447d577b023907cc67492b644a4
nishit <nishitmohanan@gmail.com>
2020-06-03 22:55:30 +0530

    Added dark mode toggle

--- utils.js
+++ utils.js
@@ -3,6 +3,18 @@ const test = [
   "Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this.",
   "If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me. "
 ];
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+    }
+});
 
 document.addEventListener("DOMContentLoaded", function(event) {
   const test_area = document.getElementsByClassName("test_area")[0];
@@ -72,3 +84,4 @@ function set_test(test_number) {
     test_area.appendChild(span);
   }
 }
+
